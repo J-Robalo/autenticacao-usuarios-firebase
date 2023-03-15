5b20027646b864838e7961750bf01e2db561cc15
João Robalo <joao27of@gmail.com>
2023-03-15 15:38:50 -0300

    Iniciando JavaScript do projeto

---
+++ js/form.js
@@ -0,0 +1,4 @@
+document.getElementById("reg-btn").addEventListener("click", function () {
+  document.getElementById("register-div").style.display = "inline";
+  document.getElementById("login-div").style.display = "none";
+});
